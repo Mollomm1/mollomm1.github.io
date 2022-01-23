c2b66b777cc9f8554e35bb5d83baf64273e7f86a
mollomm1 <68751598+Mollomm1@users.noreply.github.com>
2022-01-23 17:27:11 +0100

    Update https-extension.js

--- turbowarp/https-extension.js
+++ turbowarp/https-extension.js
@@ -22,26 +22,22 @@ function httpGet(theUrl)
 }
 
 class MyExtension {
-  /**
-   * Scratch will call this method *once* when the extension loads.
-   * This method's job is to tell Scratch things like the extension's ID, name, and what blocks it supports.
-   */
+  // VARIABLES //
+  var _text = "";
+  var _status_code = "500";
+  
+  ///////////////
+  
   getInfo() {
     return {
-      // `id` is the internal ID of the extension
-      // It should never change!
-      // If you choose to make an actual extension, please change this to something else.
-      // Only the characters a-z and 0-9 can be used. No spaces or special characters.
-      id: 'https',
+      id: 'httpsv2',
 
-      // `name` is what the user sees in the toolbox
-      // It can be changed without breaking projects.
-      name: 'Mollomm1\'s HTTPS (legacy)',
+      name: 'Mollomm1\'s HTTPS (V2.0 WIP)',
 
       blocks: [
         {
           opcode: 'GetHTTPS',
-          blockType: Scratch.BlockType.REPORTER,
+          blockType: Scratch.BlockType.COMMAND,
           text: 'Request Get (not proxied) [ONE]',
           arguments: {
             ONE: {
@@ -52,7 +48,7 @@ class MyExtension {
         },
         {
           opcode: 'GetHTTPSproxied',
-          blockType: Scratch.BlockType.REPORTER,
+          blockType: Scratch.BlockType.COMMAND,
           text: 'Request Get (Proxied) [ONE]',
           arguments: {
             ONE: {
@@ -64,10 +60,6 @@ class MyExtension {
       ]
     };
   }
-
-  /**
-   * Corresponds to `opcode: 'hello'` above
-   */
 
   
   GetHTTPS(args) {
